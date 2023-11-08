2a2e2617992ab5f6b11b29a70e88679798aedf7c
Mohammed Ashraf <mohamedasharf55@gmail.com>
2023-11-09 01:21:43 +0200

    add four lamp

--- LambGame/app/src/main/java/com/example/lambgame/MainActivity.kt
+++ LambGame/app/src/main/java/com/example/lambgame/MainActivity.kt
@@ -7,19 +7,32 @@ import android.widget.Button
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
-    private var lampBroken = false
     private var lampOn = false
+    private var lampNum = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var lamp = findViewById<ImageView>(R.id.lamp1)
         val image = findViewById<ImageView>(R.id.image)
         val button = findViewById<Button>(R.id.btn)
         image.setOnLongClickListener {
             val mediaPlayer = MediaPlayer.create(this, R.raw.broke)
-            image.setImageResource(R.drawable.lamba_broke)
-            lampBroken = true
+            image.setImageResource(R.drawable.lamba_off)
+            lamp.setImageResource(R.drawable.lamba_broke)
             mediaPlayer.start()
-            button.visibility = View.VISIBLE
+            if (lampNum == 4){
+                button.visibility = View.VISIBLE
+            }
+            lampNum++
+            lampOn = false
+            when(lampNum)
+            {
+                1 -> lamp = findViewById(R.id.lamp1)
+                2 -> lamp = findViewById(R.id.lamp2)
+                3 -> lamp = findViewById(R.id.lamp3)
+                4 -> lamp = findViewById(R.id.lamp4)
+                else -> lamp = findViewById(R.id.lamp1)
+            }
             true
         }
     }
@@ -27,16 +40,27 @@ class MainActivity : AppCompatActivity() {
     {
         val mediaPlayer = MediaPlayer.create(this,R.raw.click)
         val imageView = view as ImageView
-
-        if (!lampOn && !lampBroken)
+        var lamp = findViewById<ImageView>(R.id.lamp1)
+        when(lampNum)
+        {
+            1 -> lamp = findViewById(R.id.lamp1)
+            2 -> lamp = findViewById(R.id.lamp2)
+            3 -> lamp = findViewById(R.id.lamp3)
+            4 -> lamp = findViewById(R.id.lamp4)
+        }
+        if (!lampOn && lampNum != 5)
         {
             imageView.setImageResource(R.drawable.lamba_on)
+            lamp.setImageResource(R.drawable.lamba_on)
+
             lampOn = !lampOn
             mediaPlayer.start()
         }
-        else if (lampOn && !lampBroken)
+        else if (lampOn && lampNum != 5)
         {
             imageView.setImageResource(R.drawable.lamba_off)
+            lamp.setImageResource(R.drawable.lamba_off)
+
             lampOn = !lampOn
             mediaPlayer.start()
         }
@@ -46,8 +70,12 @@ class MainActivity : AppCompatActivity() {
         val imageView = findViewById<ImageView>(R.id.image)
         val button = view as Button
         imageView.setImageResource(R.drawable.lamba_off)
+        findViewById<ImageView>(R.id.lamp1).setImageResource(R.drawable.lamba_off)
+        findViewById<ImageView>(R.id.lamp2).setImageResource(R.drawable.lamba_off)
+        findViewById<ImageView>(R.id.lamp3).setImageResource(R.drawable.lamba_off)
+        findViewById<ImageView>(R.id.lamp4).setImageResource(R.drawable.lamba_off)
         lampOn = false
-        lampBroken = false
+        lampNum = 1
         button.visibility = View.INVISIBLE
     }
 }
